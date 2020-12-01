1a8ec442a74ec30c8c52e652413b5ad7078319d1
FranciscoJRamirez <42820012+FranciscoJRamirez@users.noreply.github.com>
2020-11-30 23:40:49 -0700

    Practica con eventos y prueba con circulos

--- practica12_layout/main.py
+++ practica12_layout/main.py
@@ -1,49 +1,69 @@
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
-from kivy.graphics import Color, Rectangle
+from kivy.core.window import Window
+from kivy.graphics import Color, Rectangle, Line
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.stencilview import StencilView
 from random import random
 from functools import partial
 
 
+class StencilWidget(StencilView):
+
+    def on_touch_down(self, touch):
+        self.pos = touch.pos
+        self.size = (1, 1)
+
+    def on_touch_move(self, touch):
+        self.size = (touch.x - touch.ox, touch.y - touch.oy)
+
+
 class Practica12(App):
 
-    def masCuadros(self, label, wid, count, *largs):
+    def dibujarCuadros(self, label, wid, count, *largs):
         label.text = str(int(label.text) + count)
         with wid.canvas:
             for x in range(count):
-                Color(random(), 10, 10, mode='hsv')
-                Rectangle(pos=(random() * wid.width + wid.x,
-                               random() * wid.height + wid.y), size=(20, 20))
+                Color(random(), 1, 1, mode='hsv')
+                #Rectangle(pos=(random() * wid.width + wid.x,
+                 #              random() * wid.height + wid.y), size=(10, 10))
+                Line(pos=(random() * wid.width + wid.x,
+                               random() * wid.height + wid.y), circle=(380,145,2),width=2)
+
+    def nuevoBloque(self, wid, *largs):
+        wid.pos = (0, 0)
+        wid.size = Window.size
 
     def limpiarCuadros(self, label, wid, *largs):
         label.text = '0'
         wid.canvas.clear()
 
     def build(self):
-        wid = Widget()
+        wid = StencilWidget(size_hint=(None, None), size=Window.size)
 
         label = Label(text='0')
 
-        addTen = Button(text='Dibujar 10',
-                            on_press=partial(self.masCuadros, label, wid, 10))
+        dibujar = Button(text='Dibujar rectangulos')
+        dibujar.bind(on_press=partial(self.dibujarCuadros, label, wid, 200))
 
-        addFifty = Button(text='Dibujar 50',
-                            on_press=partial(self.masCuadros, label, wid, 50))
+        limpiar = Button(text='Reset Rectangles')
+        limpiar.bind(on_press=partial(self.limpiarCuadros, label, wid))
 
-        limpiar = Button(text='Reset',
-                           on_press=partial(self.limpiarCuadros, label, wid))
+        nuevo = Button(text='Reset Stencil')
+        nuevo.bind(on_press=partial(self.nuevoBloque, wid))
 
         layout = BoxLayout(size_hint=(1, None), height=50)
-        layout.add_widget(addTen)
-        layout.add_widget(addFifty)
+        layout.add_widget(dibujar)
         layout.add_widget(limpiar)
+        layout.add_widget(nuevo)
         layout.add_widget(label)
 
         root = BoxLayout(orientation='vertical')
-        root.add_widget(wid)
+        rfl = FloatLayout()
+        rfl.add_widget(wid)
+        root.add_widget(rfl)
         root.add_widget(layout)
 
         return root
